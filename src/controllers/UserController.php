a52fa08c1967541d73be55514737860fe37023c3
Michael Matheus <michaelmatheus2001@gmail.com>
2023-02-13 07:42:52 -0300

    feat(controller): create user validate

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -2,11 +2,36 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\models\User;
 
 class UserController extends Controller {
 
     public function add() {
-        echo "novo";
+        $this->render('add');
+    }
+
+    public function addAction() {
+        $name = filter_input(INPUT_POST, 'name');
+        $email = filter_input(INPUT_POST, 'email');
+
+        if($name && $email){
+            $data = User::select()->where('email', $email)->execute();
+
+            if(count($data) === 0){
+
+                User::insert([
+                    'name' => $name,
+                    'email' => $email
+                ])->execute();
+
+                echo 'inseriu';
+                exit;
+
+                $this->redirect('/');
+            } 
+        } 
+
+        $this->redirect('/new');
     }
 
 }

689758a88ea557c6dfc1fbe8c419ffd4a2411591
Michael Matheus <michaelmatheus2001@gmail.com>
2023-02-13 07:46:17 -0300

    feat: user add routes

--- src/routes.php
+++ src/routes.php
@@ -4,4 +4,6 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
+
 $router->get('/new', 'UserController@add');
+$router->post('/new', 'UserController@addAction');

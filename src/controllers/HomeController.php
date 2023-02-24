092b439a9392961ea35acc84b6dc179631b88127
Michael Matheus <michaelmatheus2001@gmail.com>
2023-02-24 06:11:21 -0300

    feat: user posts

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -6,8 +6,23 @@ use \core\Controller;
 class HomeController extends Controller {
 
     public function index() {
-        $title = 'Página Home';
-        $this->render('home', ['title' => $title]);
+        
+        $name = 'Michael';
+        $age = 21;
+        $posts = [
+            ['title' => 'Título de teste 1', 'body' => 'corpo de teste 1'],
+            ['title' => 'Título de teste 2', 'body' => 'corpo de teste 2'],
+            ['title' => 'Título de teste 3', 'body' => 'corpo de teste 3'],
+            ['title' => 'Título de teste 4', 'body' => 'corpo de teste 4'],
+            ['title' => 'Título de teste 5', 'body' => 'corpo de teste 5'],
+            ['title' => 'Título de teste 6', 'body' => 'corpo de teste 6'],
+        ];
+
+        $this->render('home', [
+            'name' => $name,
+            'age' => $age,
+            'posts' => $posts,
+        ]);
     }
 
 }
